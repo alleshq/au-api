8ea1ce975ce13db610b66821f10dcbcff88a2dc5
Archie Baer <archie@abaer.dev>
2020-03-23 17:16:48 +0000

    Transactions endpoint

--- api/v1/_.js
+++ api/v1/_.js
@@ -3,4 +3,5 @@ const auth = require("../../util/auth");
 
 const router = express.Router();
 router.get("/account", auth, require("./account"));
+router.get("/transactions", auth, require("./transactions"));
 module.exports = router;
---
+++ api/v1/transactions.js
@@ -0,0 +1,46 @@
+const {Op} = require("sequelize");
+
+module.exports = async (req, res) => {
+    //Transactions in the last 30 days
+    const since = new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 30);
+
+    //Get in/out transactions
+    const inTransactions = await req.account.getInTransactions({
+        where: {
+            createdAt: {
+                [Op.gte]: since
+            }
+        }
+    });
+    const outTransactions = await req.account.getOutTransactions({
+        where: {
+            createdAt: {
+                [Op.gte]: since
+            }
+        }
+    });
+
+    //Concat into transactions
+    var transactions = inTransactions.concat(outTransactions);
+
+    //Sort
+    transactions.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+    //Map
+    transactions = transactions.map(t => ({
+        id: t.id,
+        in: t.toId === req.account.id,
+        participant: t.toId === req.account.id ? t.fromId : t.toId,
+        amount: t.amount,
+        fee: t.fee,
+        meta: t.meta,
+        redirect: t.redirect,
+        refunded: t.refunded,
+        createdAt: t.createdAt
+    }))
+
+    //Response
+    res.json({
+        transactions
+    });
+};
